69e886b98326bf1364cc61990fed1fffa551e59a
edmancota <edmancota2017@gmail.com>
2022-05-15 15:16:15 -0600

    husky testing 3

--- src/components/SizeSelector.tsx
+++ src/components/SizeSelector.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/function-component-definition */
 import React from "react";
 
 const possibleSizes = ["XS", "S", "M", "L", "XL", "XXL"];

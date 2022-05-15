ca1c9a73803115dcff3a45f79c7ea8edb3911ccd
edmancota <edmancota2017@gmail.com>
2022-05-15 14:13:33 -0600

    Initial commit

---
+++ src/components/SizeSelector.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/function-component-definition */
+import React from "react";
+
+const possibleSizes = ["XS", "S", "M", "L", "XL", "XXL"];
+
+interface Props {
+  selectedSize: string;
+  onSizeChange: (size: string) => void;
+}
+
+const SizeSelector: React.FC<Props> = ({ selectedSize, onSizeChange }) => (
+  <div style={{ display: "flex" }}>
+    {possibleSizes.map((size) => (
+      <button
+        type="button"
+        key={size}
+        className={selectedSize === size ? "active" : ""}
+        onClick={() => onSizeChange(size)}
+      >
+        {size}
+      </button>
+    ))}
+  </div>
+);
+
+export default SizeSelector;
